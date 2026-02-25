a6fcbf69050507ea3bb397a8f3dca4b7b4cc34b8
Pintu241 <yaduvanasipintu241@gmail.com>
2026-02-25 14:30:43 +0530

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-// Bypass strict SSL/TLS network interception for local development
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const express = require('express');
 const cors = require('cors');
